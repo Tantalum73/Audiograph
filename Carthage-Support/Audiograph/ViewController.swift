96107c61d0e3845733912c7cb8fd0590cfd35792
Andreas <developer@anerma.de>
2020-06-15 14:48:20 +0200

    Added Carthage project file.

---
+++ Carthage-Support/Audiograph/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Audiograph
+//
+//  Created by Andreas Neusüß on 15.06.20.
+//  Copyright © 2020 Anerma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

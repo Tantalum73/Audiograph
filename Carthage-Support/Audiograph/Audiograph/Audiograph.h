7cc559c07c4ecf757fb8e63ee4df617706795e12
Andreas <developer@anerma.de>
2020-06-15 15:32:50 +0200

    Added dynamic library for carthage support

---
+++ Carthage-Support/Audiograph/Audiograph/Audiograph.h
@@ -0,0 +1,19 @@
+//
+//  Audiograph.h
+//  Audiograph
+//
+//  Created by Andreas Neusüß on 15.06.20.
+//  Copyright © 2020 Anerma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Audiograph.
+FOUNDATION_EXPORT double AudiographVersionNumber;
+
+//! Project version string for Audiograph.
+FOUNDATION_EXPORT const unsigned char AudiographVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Audiograph/PublicHeader.h>
+
+
